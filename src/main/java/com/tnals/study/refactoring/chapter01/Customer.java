34add245f587b9c87365a58296fe9f833e3c79f6
tnals <tnals1422@gmail.com>
2021-10-09 17:10:22 +0900

    포인트 계산로직 Rental 클래스로 메서드 생성하여 이동

--- src/main/java/com/tnals/study/refactoring/chapter01/Customer.java
+++ src/main/java/com/tnals/study/refactoring/chapter01/Customer.java
@@ -32,13 +32,8 @@ public class Customer {
         while (rentals.hasMoreElements()) {
             Rental each = (Rental) rentals.nextElement();
 
-            // 포인트 (frequent renter points) 추가
-            frequentRenterPoints ++;
-
-            // 최신 (new release)을  이틀 이상 대여하는 경우 추가 포인트 제공
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1) {
-                frequentRenterPoints ++;
-            }
+            // 포인트 계산
+            frequentRenterPoints += each.getFrequentRenterPoints();
 
             // 이 대여에 대한 요금 계산결과 표시
             result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(each.getCharge()) + "\n";
