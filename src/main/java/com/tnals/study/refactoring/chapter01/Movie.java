9c2456211b22a3ab3f417abf8dca123a6d3db956
tnals <tnals1422@gmail.com>
2021-10-09 16:04:29 +0900

    chapter01 예제

---
+++ src/main/java/com/tnals/study/refactoring/chapter01/Movie.java
@@ -0,0 +1,31 @@
+package com.tnals.study.refactoring.chapter01;
+
+/**
+ * .
+ */
+public class Movie {
+
+    public static final int CHILDREN = 2;
+    public static final int REGULAR = 0;
+    public static final int NEW_RELEASE = 1;
+
+    private String _title;
+    private int _priceCode;
+
+    public Movie(String title, int priceCode) {
+        _title = title;
+        _priceCode = priceCode;
+    }
+
+    public String getTitle() {
+        return _title;
+    }
+
+    public int getPriceCode() {
+        return _priceCode;
+    }
+
+    public void setPriceCode(int arg) {
+        _priceCode = arg;
+    }
+}
---
+++ src/main/java/com/tnals/study/refactoring/chapter01/Rental.java
@@ -0,0 +1,23 @@
+package com.tnals.study.refactoring.chapter01;
+
+/**
+ * .
+ */
+public class Rental {
+
+    private final Movie _movie;
+    private final int _daysRented;
+
+    public Rental(Movie movie, int daysRented) {
+        _movie = movie;
+        _daysRented = daysRented;
+    }
+
+    public int getDaysRented() {
+        return _daysRented;
+    }
+
+    public Movie getMovie() {
+        return _movie;
+    }
+}
