17555b365126a0c48579f31651c21c20fc9f4ea1
tnals1422 <51726564+tnals1422@users.noreply.github.com>
2021-10-09 14:19:47 +0900

    Add files via upload

---
+++ src/main/java/com/tnals/study/refactoring/RefactoringApplication.java
@@ -0,0 +1,13 @@
+package com.tnals.study.refactoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefactoringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RefactoringApplication.class, args);
+    }
+
+}
